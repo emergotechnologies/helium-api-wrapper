12e77c94479ed8e8c5ac77435fc10cb295f59fff
Matthias Oberleitner <2110837034@stud.fh-kufstein.ac.at>
2022-12-03 13:11:00 +0100

    - remove poetry install from testing

--- noxfile.py
+++ noxfile.py
@@ -167,7 +167,6 @@ def tests(session: Session) -> None:
     """Run the test suite."""
     session.install(".")
     session.install("coverage[toml]", "pytest", "pygments")
-    session.run("poetry", "install")
     try:
         session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
     finally:
