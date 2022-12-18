f42c5a1e3d0d0b206763dd20a662b4f381270760
Fabian Kainz <fabian@kainz.in>
2022-12-18 12:35:27 +0100

    ~ reduce cli command complexity

--- src/helium_api_wrapper/__main__.py
+++ src/helium_api_wrapper/__main__.py
@@ -8,13 +8,17 @@
 
 """
 
+from typing import List
+
 import click
 
 from helium_api_wrapper.challenges import get_challenges
 from helium_api_wrapper.challenges import get_challenges_by_address
 from helium_api_wrapper.challenges import load_challenge_data
+from helium_api_wrapper.DataObjects import ChallengeResolved
 from helium_api_wrapper.DataObjects import Device
 from helium_api_wrapper.DataObjects import Event
+from helium_api_wrapper.DataObjects import Hotspot
 from helium_api_wrapper.devices import get_device_by_uuid
 from helium_api_wrapper.devices import get_last_event
 from helium_api_wrapper.devices import get_last_integration
@@ -25,85 +29,36 @@ from helium_api_wrapper.ResultHandler import write
 
 @click.command()
 @click.option("--address", type=str, help="Address of the hotspot")
-@click.option(
-    "--file_format",
-    default="pickle",
-    type=str,
-    help="Defines the format for the output file.",
-)
-@click.option(
-    "--file_name", default="hotspot", type=str, help="Defines the name of the file."
-)
-@click.option(
-    "--path", default="./data", type=str, help="Defines the path for the output file."
-)
 @click.version_option(version="0.1")
-def get_hotspot(address: str, file_format: str, file_name: str, path: str) -> None:
+def get_hotspot(address: str) -> Hotspot:
     """This function returns a Hotspot for a given address."""
     if address:
         hotspot = get_hotspot_by_address(address)
     else:
         raise ValueError("No address given")
 
-    write(
-        data=hotspot,
-        file_format=file_format,
-        file_name=file_name,
-        path=path,
-    )
+    print(hotspot[0])
+    return hotspot[0]
 
 
 @click.command()
 @click.option("--n", type=int, help="Nr. of pages to load. 1 page = 1000 hotspots")
-@click.option(
-    "--file_format",
-    default="pickle",
-    type=str,
-    help="Defines the format for the output file.",
-)
-@click.option(
-    "--file_name", default="hotspots", type=str, help="Defines the name of the file."
-)
-@click.option(
-    "--path", default="./data", type=str, help="Defines the path for the output file."
-)
 @click.version_option(version="0.1")
-def load_hotspots(n: int, file_format: str, file_name: str, path: str) -> None:
+def load_hotspots(n: int) -> List[Hotspot]:
     """This function returns a given number of random Hotspots."""
     hotspots = get_hotspots(n)
-    write(
-        data=hotspots,
-        file_format=file_format,
-        file_name=file_name,
-        path=path,
-    )
+    print(hotspots[:3])
+    return hotspots
 
 
 @click.command()
 @click.option("--address", type=str, help="Address of the hotspot")
-@click.option(
-    "--file_format",
-    default="pickle",
-    type=str,
-    help="Defines the format for the output file.",
-)
-@click.option(
-    "--file_name", default="challenges", type=str, help="Defines the name of the file."
-)
-@click.option(
-    "--path", default="./data", type=str, help="Defines the path for the output file."
-)
 @click.version_option(version="0.1")
-def get_challenges_for_hotspot(
-    address: str, file_format: str, file_name: str, path: str
-) -> None:
+def get_challenges_for_hotspot(address: str) -> List[ChallengeResolved]:
     """This function returns a list of challenges for a given hotspot."""
-    write(
-        get_challenges_by_address(address),
-        file_format=file_format,
-        file_name=file_name,
-        path=path,
-    )
+    challenges = get_challenges_by_address(address)
+    print(challenges[:3])
+    return challenges
 
 
 @click.command()
